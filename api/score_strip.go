9cd48c77d6a9bfb6d0c245ef0278d2b684109c2f
Jay Zheng <jay.j.zheng@gmail.com>
2019-04-26 11:20:38 -0700

    create api.Scores type

--- api/score_strip.go
+++ api/score_strip.go
@@ -27,31 +27,33 @@ func ScoreStripURL(in ScoreStripURLInput) string {
 	return u.String()
 }
 
-type ScoreStrip struct {
-	Scores struct {
-		Year int    `xml:"y,attr"`
-		Week int    `xml:"w,attr"`
-		Type string `xml:"t,attr"`
-		GD   int    `xml:"gd,attr"` // ?
-		BPH  string `xml:"bph,attr"`
+type Scores struct {
+	Year int    `xml:"y,attr"`
+	Week int    `xml:"w,attr"`
+	Type string `xml:"t,attr"`
+	GD   int    `xml:"gd,attr"` // ?
+	BPH  string `xml:"bph,attr"`
 
-		Games []struct {
-			ID              string `xml:"eid,attr"`
-			GSIS            string `xml:"gsis,attr"`
-			Day             string `xml:"d,attr"`
-			Time            string `xml:"t,attr"`
-			Quarter         string `xml:"q,attr"`
-			QuarterTimeLeft string `xml:"k,attr"`
-			Home            string `xml:"h,attr"`
-			HomeNickName    string `xml:"hnn,attr"`
-			HomeScore       int    `xml:"hs,attr"`
-			Visitor         string `xml:"v,attr"`
-			VisitorNickName string `xml:"vnn,attr"`
-			VisitorScore    int    `xml:"vs,attr"`
-			Redzone         int    `xml:"rz,attr"`
-			Possesion       string `xml:"p,attr"`
-			GA              string `xml:"ga,attr"` // ?
-			GameType        string `xml:"gt,attr"`
-		} `xml:"g"`
-	} `xml:"gms"`
+	Games []struct {
+		ID              string `xml:"eid,attr"`
+		GSIS            string `xml:"gsis,attr"`
+		Day             string `xml:"d,attr"`
+		Time            string `xml:"t,attr"`
+		Quarter         string `xml:"q,attr"`
+		QuarterTimeLeft string `xml:"k,attr"`
+		Home            string `xml:"h,attr"`
+		HomeNickName    string `xml:"hnn,attr"`
+		HomeScore       int    `xml:"hs,attr"`
+		Visitor         string `xml:"v,attr"`
+		VisitorNickName string `xml:"vnn,attr"`
+		VisitorScore    int    `xml:"vs,attr"`
+		Redzone         int    `xml:"rz,attr"`
+		Possesion       string `xml:"p,attr"`
+		GA              string `xml:"ga,attr"` // ?
+		GameType        string `xml:"gt,attr"`
+	} `xml:"g"`
+}
+
+type ScoreStrip struct {
+	Scores Scores `xml:"gms"`
 }
